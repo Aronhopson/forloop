1e017c50920d689e9b53cca5959d49c42aa783ae
aronhopson <aronhopson97@gmail.com>
2019-07-09 23:27:17 +0530

    forloop

---
+++ forloop.js
@@ -0,0 +1,34 @@
+/*function isEven(num){
+	if(num % 2===0){
+		return true;
+	}
+	else{
+		retuen false
+	}
+}
+
+
+function fractional(num){
+	var result = 1;
+	for (var i=2; i<=num; i++){
+		result = result*=1;
+		return result
+	}
+}
+
+function KababToSnake(str){
+	var newStr = str.replace(/@/g,  "%");
+	return newStr;
+}*/
+
+var num = 8;
+function doMath(){
+	num+=1;
+	if(num % 5 ==0){
+		return true
+	}
+	else{
+		return false
+	}
+}
+num+=1;
